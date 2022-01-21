03d84d7cf656b1a075cae2735109f54b8f6df75f
Jpk7948 <jpk7948@rit.edu>
2022-01-20 21:01:54 -0500

    Shift Decryption

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        Console.Write("What type of cipher would you like to decrypt? ");
+        string response = Console.ReadLine();
+        Console.Write("Please enter the text to be decrypted: ");
+        string ciphertext = Console.ReadLine();
+
+        if(response == "shift") Shift_Decryption(ciphertext);
+    }
+
+    public static void Shift_Decryption(string ciphertext) {
+        for(int i = 1; i < 26; i++) {
+            string plaintext = "";
+
+            ciphertext.ToList().ForEach(x=>{
+                if((int)x == 32) plaintext += x;
+                else plaintext += (char)(x+i-122+96);
+            });
+
+            Console.WriteLine($"Attempt {i}: {plaintext}");
+        }
+    }
+}
