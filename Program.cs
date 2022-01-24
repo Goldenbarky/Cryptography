96fc3b0d7bc66ff27fd8a96a6648637407f8c8f8
Jpk7948 <jpk7948@rit.edu>
2022-01-24 16:38:22 -0500

    LFSR progress

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿class Program {
+﻿using System.Collections;
+class Program {
     public static void Main(string[] args) {
         Console.Write("What type of cipher would you like to decrypt? ");
         string response = Console.ReadLine();
@@ -19,5 +20,35 @@
 
             Console.WriteLine($"Attempt {i}: {plaintext}");
         }
+    }
+
+    public static void Linear_Shift_Register(string initial_keystream) {
+        Console.Write("Please enter the P values (010): ");
+        string coefficients = Console.Read();
+        bool[] paths = Bit_String(coefficients);
+
+        Stack<bool> registers = new Stack<bool>();
+        foreach(bool bit in Bit_String(initial_keystream)) {
+            registers.Push(bit);
+        }
+
+        string keystream;
+
+        for(int i = 0; i < Math.Pow(2, initial_keystream.Length); i++) {
+            keystream += (registers.Peek);
+
+
+        }
+    }
+
+    public static bool[] Bit_String(string str) {
+        bool[] bits = new bool[str.Length];
+
+        for(int i = 0; i < str.Length; i++) {
+            if(str[i] != "0" && str[i] != "1") return null;
+            bits[i] = (str[i] == 1) ? true : false;
+        }
+
+        return bits;
     }
 }
