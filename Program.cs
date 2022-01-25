b3275cd98195a41f7a6e14cd3b95eba2b9880c00
Jpk7948 <jpk7948@rit.edu>
2022-01-25 11:19:42 -0500

    LFSR complete (pre-testing)

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ class Program {
         string ciphertext = Console.ReadLine();
 
         if(response == "shift") Shift_Decryption(ciphertext);
+        if(response == "lfsr") Linear_Shift_Register(ciphertext);
     }
 
     public static void Shift_Decryption(string ciphertext) {
@@ -24,29 +25,37 @@ class Program {
 
     public static void Linear_Shift_Register(string initial_keystream) {
         Console.Write("Please enter the P values (010): ");
-        string coefficients = Console.Read();
-        bool[] paths = Bit_String(coefficients);
+        string coefficients = Console.ReadLine();
+        int[] paths = Bit_String(coefficients);
 
-        Stack<bool> registers = new Stack<bool>();
-        foreach(bool bit in Bit_String(initial_keystream)) {
-            registers.Push(bit);
+        
+        Queue<int> registers = new Queue<int>();
+        foreach(int bit in Bit_String(initial_keystream)) {
+            registers.Enqueue(bit);
         }
 
-        string keystream;
+        List<int> keystream = new List<int>();
 
         for(int i = 0; i < Math.Pow(2, initial_keystream.Length); i++) {
-            keystream += (registers.Peek);
+            int prev = 0;
+            for(int j = 0; j < paths.Length; j++) {
+                prev = (paths[j] & registers.ToList()[i]) ^ prev;
+            }
+            keystream.Add(registers.Dequeue());
+            registers.Enqueue(prev);
 
-
+            registers.ToList().ForEach(x=>Console.Write(x));
+            Console.WriteLine();
         }
     }
 
-    public static bool[] Bit_String(string str) {
-        bool[] bits = new bool[str.Length];
+    //Turns a string of bits into an array of integers for bitwise operations
+    public static int[] Bit_String(string str) {
+        int[] bits = new int[str.Length];
 
         for(int i = 0; i < str.Length; i++) {
-            if(str[i] != "0" && str[i] != "1") return null;
-            bits[i] = (str[i] == 1) ? true : false;
+            if(str[i] != '0' && str[i] != '1') return null;
+            bits[i] = (str[i] == 1) ? 1 : 0;
         }
 
         return bits;
